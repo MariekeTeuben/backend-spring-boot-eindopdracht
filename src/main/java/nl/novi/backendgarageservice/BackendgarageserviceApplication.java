4f17ce84b47dbeef0fcd3121e78334fee2ff3d5a
MariekeTeuben <marieke@marenmaa.nl>
2023-08-03 16:33:05 +0200

    first setup project

---
+++ src/main/java/nl/novi/backendgarageservice/BackendgarageserviceApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.backendgarageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendgarageserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendgarageserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/backendgarageservice/BackendgarageserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.backendgarageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendgarageserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
