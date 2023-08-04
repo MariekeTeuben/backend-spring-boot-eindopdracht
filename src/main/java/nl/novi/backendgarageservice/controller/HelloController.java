63fca61236d9083588804708fc08b37221baefbf
MariekeTeuben <marieke@marenmaa.nl>
2023-08-04 15:03:48 +0200

    added HelloController

--- src/main/java/nl/novi/backendgarageservice/controller/HelloController.java
+++ src/main/java/nl/novi/backendgarageservice/controller/HelloController.java
@@ -1,4 +1,4 @@
-package nl.novi.backendgarageservice;
+package nl.novi.backendgarageservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
