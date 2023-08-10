9a238aa1596397c20889bbcc7772167d986d964a
MariekeTeuben <marieke@marenmaa.nl>
2023-08-10 12:39:36 +0200

    created Appointment class

--- src/main/java/nl/novi/backendgarageservice/model/Appointment.java
+++ src/main/java/nl/novi/backendgarageservice/model/Appointment.java
@@ -1,9 +1,56 @@
 package nl.novi.backendgarageservice.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "appointments")
 public class Appointment {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private LocalDate date;
+
+    private LocalTime time;
+
+    private String typeOfMaintenance;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getTypeOfMaintenance() {
+        return typeOfMaintenance;
+    }
+
+    public void setTypeOfMaintenance(String typeOfMaintenance) {
+        this.typeOfMaintenance = typeOfMaintenance;
+    }
 }
