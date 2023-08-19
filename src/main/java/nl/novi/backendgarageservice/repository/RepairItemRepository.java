987b14fc3f27952b3660914a989774cd1bd692e6
MariekeTeuben <marieke@marenmaa.nl>
2023-08-19 11:53:28 +0200

    created RepairItemRepository

---
+++ src/main/java/nl/novi/backendgarageservice/repository/RepairItemRepository.java
@@ -0,0 +1,7 @@
+package nl.novi.backendgarageservice.repository;
+
+import nl.novi.backendgarageservice.model.RepairItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepairItemRepository extends JpaRepository<RepairItem, Long> {
+}
