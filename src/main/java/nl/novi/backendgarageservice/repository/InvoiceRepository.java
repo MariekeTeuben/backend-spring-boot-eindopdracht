0a7d0c115c2788126b00142450364bafdabad09c
MariekeTeuben <marieke@marenmaa.nl>
2023-08-19 11:48:57 +0200

    created InvoiceRepository

---
+++ src/main/java/nl/novi/backendgarageservice/repository/InvoiceRepository.java
@@ -0,0 +1,7 @@
+package nl.novi.backendgarageservice.repository;
+
+import nl.novi.backendgarageservice.model.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+}
