0b3d03b9c5971c9e3e13e711b72097dda8713368
MariekeTeuben <marieke@marenmaa.nl>
2023-08-25 12:27:30 +0200

    updated Invoice class

--- src/main/java/nl/novi/backendgarageservice/model/Invoice.java
+++ src/main/java/nl/novi/backendgarageservice/model/Invoice.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "invoices")
 public class Invoice {
@@ -13,4 +15,41 @@ public class Invoice {
     @GeneratedValue
     private Long id;
 
+    private LocalDate date;
+
+    private Double tax;
+
+    private Double total;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
 }
