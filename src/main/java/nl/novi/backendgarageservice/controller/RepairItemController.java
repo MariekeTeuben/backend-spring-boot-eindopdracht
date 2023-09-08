b2689f65c6a3e412a42b83780e70277933c1fd65
MariekeTeuben <marieke@marenmaa.nl>
2023-09-08 10:55:41 +0200

    created mappings for RepairItem

--- src/main/java/nl/novi/backendgarageservice/controller/RepairItemController.java
+++ src/main/java/nl/novi/backendgarageservice/controller/RepairItemController.java
@@ -1,32 +1,69 @@
 package nl.novi.backendgarageservice.controller;
 
-import nl.novi.backendgarageservice.model.RepairItem;
-import nl.novi.backendgarageservice.repository.RepairItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import nl.novi.backendgarageservice.dto.RepairItemDto;
+import nl.novi.backendgarageservice.service.RepairItemService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
+import java.util.ArrayList;
+
 
 @RestController
 @RequestMapping("/repairitems")
 public class RepairItemController {
 
-    @Autowired
-    private RepairItemRepository repos;
+    private final RepairItemService repairItemService;
+
+    public RepairItemController(RepairItemService repairItemService) {
+        this.repairItemService = repairItemService;
+    }
 
     @GetMapping
-    public ResponseEntity<List<RepairItem>> getAllRepairItems() {
-        return ResponseEntity.ok(repos.findAll());
+    public ResponseEntity<ArrayList<RepairItemDto>> getAllRepairItems() {
+        return new ResponseEntity(repairItemService.getAllRepairItems(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RepairItemDto> getRepairItemById(@PathVariable Long id) {
+        return new ResponseEntity(repairItemService.getRepairItemById(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<RepairItem> createRepairItem(@RequestBody RepairItem repairitem) {
-        repos.save(repairitem);
-        URI uri = URI.create(ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/" + repairitem.getId()).toUriString());
-        return ResponseEntity.created(uri).body(repairitem);
+    public ResponseEntity<Object> createRepairItem(@Validated @RequestBody RepairItemDto repairItemDto, BindingResult br) {
+        if (br.hasFieldErrors()) {
+            StringBuilder sb = new StringBuilder();
+            for (FieldError fe : br.getFieldErrors()) {
+                sb.append(fe.getField() + ": ");
+                sb.append(fe.getDefaultMessage());
+                sb.append("\n");
+            }
+            return ResponseEntity.badRequest().body(sb.toString());
+        }
+        else {
+            Long newId = repairItemService.createRepairItem(repairItemDto);
+
+            URI uri = URI.create(ServletUriComponentsBuilder
+                    .fromCurrentRequest().path("/" + newId).toUriString());
+
+            repairItemDto.id = newId;
+
+            return ResponseEntity.created(uri).body(repairItemDto);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateRepairItem(@PathVariable Long id, @RequestBody RepairItemDto repairItemDto) {
+        return ResponseEntity.ok(repairItemService.updateRepairItem(id, repairItemDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteRepairItem(@PathVariable Long id) {
+        return ResponseEntity.ok(repairItemService.deleteRepairItem(id));
     }
 }
---
+++ src/main/java/nl/novi/backendgarageservice/service/RepairItemService.java
@@ -0,0 +1,101 @@
+package nl.novi.backendgarageservice.service;
+
+import nl.novi.backendgarageservice.dto.RepairItemDto;
+import nl.novi.backendgarageservice.exception.ResourceNotFoundException;
+import nl.novi.backendgarageservice.model.RepairItem;
+import nl.novi.backendgarageservice.repository.RepairItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RepairItemService {
+    private final RepairItemRepository repairItemRepos;
+
+    public RepairItemService(RepairItemRepository repairItemRepos) {
+        this.repairItemRepos = repairItemRepos;
+    }
+
+    public RepairItemDto getRepairItemById(Long id) {
+        RepairItem repairItem = repairItemRepos.findById(id).orElseThrow(() -> new ResourceNotFoundException("Repair item cannot be found"));
+
+        RepairItemDto repairItemDto = new RepairItemDto();
+        repairItemDto.id = repairItem.getId();
+        repairItemDto.itemName = repairItem.getItemName();
+        repairItemDto.itemType = repairItem.getItemType();
+        repairItemDto.itemDescription = repairItem.getItemDescription();
+        repairItemDto.itemQuantity = repairItem.getItemQuantity();
+        repairItemDto.itemPrice = repairItem.getItemPrice();
+
+        return repairItemDto;
+    }
+
+    public List<RepairItemDto> getAllRepairItems() {
+        ArrayList<RepairItemDto> repairItemDtoList = new ArrayList<>();
+        Iterable<RepairItem> allRepairItems = repairItemRepos.findAll();
+        for (RepairItem repairItem: allRepairItems) {
+            RepairItemDto repairItemDto = new RepairItemDto();
+
+            repairItemDto.id = repairItem.getId();
+            repairItemDto.itemName = repairItem.getItemName();
+            repairItemDto.itemType = repairItem.getItemType();
+            repairItemDto.itemDescription = repairItem.getItemDescription();
+            repairItemDto.itemQuantity = repairItem.getItemQuantity();
+            repairItemDto.itemPrice = repairItem.getItemPrice();
+
+            repairItemDtoList.add(repairItemDto);
+        }
+
+        if (repairItemDtoList.size() == 0) {
+            throw new ResourceNotFoundException("Repair Items cannot be found");
+        }
+
+        return repairItemDtoList;
+
+    }
+
+    public Long createRepairItem(RepairItemDto repairItemDto) {
+        RepairItem repairItem = new RepairItem();
+
+        repairItem.setItemName(repairItemDto.itemName);
+        repairItem.setItemType(repairItemDto.itemType);
+        repairItem.setItemDescription(repairItemDto.itemDescription);
+        repairItem.setItemQuantity(repairItemDto.itemQuantity);
+        repairItem.setItemPrice(repairItemDto.itemPrice);
+
+        repairItemRepos.save(repairItem);
+
+        return repairItem.getId();
+    }
+
+    public Object updateRepairItem(Long id, RepairItemDto repairItemDto) {
+        Optional<RepairItem> repairItem = repairItemRepos.findById(id);
+        if (repairItem.isEmpty()) {
+            throw new ResourceNotFoundException("No repair item with id:" + id);
+        } else {
+            RepairItem updatedRepairItem = repairItem.get();
+            updatedRepairItem.setItemName(repairItemDto.itemName);
+            updatedRepairItem.setItemType(repairItemDto.itemType);
+            updatedRepairItem.setItemDescription(repairItemDto.itemDescription);
+            updatedRepairItem.setItemQuantity(repairItemDto.itemQuantity);
+            updatedRepairItem.setItemPrice(repairItemDto.itemPrice);
+            repairItemRepos.save(updatedRepairItem);
+
+            return updatedRepairItem;
+        }
+    }
+
+    public Object deleteRepairItem(Long id) {
+        Optional<RepairItem> optionalRepairItem = repairItemRepos.findById(id);
+        if (optionalRepairItem.isEmpty()) {
+            throw new ResourceNotFoundException("Repair item cannot be found");
+        } else {
+            RepairItem repairItem = optionalRepairItem.get();
+            repairItemRepos.delete(repairItem);
+            return "Repair item deleted successfully";
+        }
+    }
+}
+
